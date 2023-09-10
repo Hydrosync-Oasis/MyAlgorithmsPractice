0cd789e388e304a0133d177e3f40227b59e8a827
HaibaraAi-Sherry <ConanMarisa@qq.com>
2023-09-10 21:40:59 +0800

    update .gitignore, update project references

--- 算法/Program.cs
+++ 算法/Program.cs
@@ -12,7 +12,7 @@ namespace Algorithm {
 
             //Test();
             var res3 =
-            Exercises.MinRefuelStops(1000, 299, Parse2DArray("[[13,21],[26,115],[100,47],[225,99],[299,141],[444,198],[608,190],[636,157],[647,255],[841,123]]"));
+            Exercises.MinRefuelStops(1000, 299, [[13,21],[26,115],[100,47],[225,99],[299,141],[444,198],[608,190],[636,157],[647,255],[841,123]]);
             sw.Stop();
             Console.WriteLine(res3);
             //Check();
