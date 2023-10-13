c2f791f2c5c94dee7791b38905494f4bafa2cc9c
HaibaraAi-Sherry <ConanMarisa@qq.com>
2023-10-14 00:15:35 +0800

    3x3华容道

---
+++ Algo4/3x3Search.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo4 {
+    internal class _3x3Search {
+        public delegate int EvaluationFunc(State state);
+        EvaluationFunc func;
+        public static State rightState = new((int[][])[[1, 2, 3], [4, 5, 6], [7, 8, 0]]);
+
+
+        public class State {
+            private int[] state = new int[9];
+            public State(int[][] state) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        this[i, j] = state[i][j];
+                    }
+                }
+            }
+
+            public State(int[] state) {
+                this.state = state;
+            }
+
+            public long GetCode() {
+                long number = 0;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        number *= 10;
+                        number += this[i, j];
+                    }
+                }
+                return number;
+            }
+
+            public List<State> GenNext() {
+                int x = -1, y = -1;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (this[i, j] == 0) {
+                            x = i;
+                            y = j;
+                            break;
+                        }
+                    }
+                }
+                List<State> ans = new();
+                if (x != 0) {
+                    (this[x, y], this[x - 1, y]) = (this[x - 1, y], this[x, y]);
+                    ans.Add(this.Copy());
+                    (this[x, y], this[x - 1, y]) = (this[x - 1, y], this[x, y]);
+                }
+                if (x != 2) {
+                    (this[x, y], this[x + 1, y]) = (this[x + 1, y], this[x, y]);
+                    ans.Add(this.Copy());
+                    (this[x, y], this[x + 1, y]) = (this[x + 1, y], this[x, y]);
+                }
+                if (y != 0) {
+                    (this[x, y], this[x, y - 1]) = (this[x, y - 1], this[x, y]);
+                    ans.Add(this.Copy());
+                    (this[x, y], this[x, y - 1]) = (this[x, y - 1], this[x, y]);
+                }
+                if (y != 2) {
+                    (this[x, y], this[x, y + 1]) = (this[x, y + 1], this[x, y]);
+                    ans.Add(this.Copy());
+                    (this[x, y], this[x, y + 1]) = (this[x, y + 1], this[x, y]);
+                }
+                return ans;
+            }
+
+            public static implicit operator State(int[][] input) => new(input);
+
+            public int this[int x, int y] {
+                get => state[3 * x + y];
+                set => state[3 * x + y] = value;
+            }
+
+            public override int GetHashCode() => GetCode().GetHashCode();
+
+            public State Copy() {
+                int[] copy = new int[9];
+                state.CopyTo(copy, 0);
+                return new(copy);
+            }
+
+            public override bool Equals(object? obj) {
+                if (obj is State s) {
+                    return state.SequenceEqual(s.state);
+                } else {
+                    return false;
+                }
+            }
+
+            public override string ToString() {
+                StringBuilder sb = new();
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        sb.Append(this[i, j]);
+                        sb.Append(' ');
+                    }
+                    sb.AppendLine();
+                }
+                return sb.ToString();
+            }
+        }
+
+        State state;
+
+        public _3x3Search(int[][] state, EvaluationFunc func) {
+            this.state = state;
+            this.func = func;
+        }
+
+        public List<State> Search() {
+            Dictionary<State, State> map = new();
+            PriorityQueue<State, int> pq = new();
+            HashSet<State> visited = new HashSet<State>() { state };
+            pq.Enqueue(state, func(state));
+            while (pq.Count > 0) {
+                var tmp = pq.Dequeue();
+                if (tmp.Equals(rightState)) {
+                    break;
+                }
+
+                List<State> nxt = tmp.GenNext();
+                foreach (var item in nxt) {
+                    if (visited.Add(item)) {
+                        map[item] = tmp;
+                        pq.Enqueue(item, func(item));
+                    }
+                }
+            }
+
+            List<State> result = new();
+            State cur = rightState;
+            while (true) {
+                result.Add(cur);
+                if (!map.ContainsKey(cur)) {
+                    break;
+                }
+                cur = map[cur];
+            }
+            result.Reverse();
+            return result;
+        }
+    }
+}
---
+++ Algo4/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Algo4;
+using System.Diagnostics;
+using static Algo4._3x3Search;
+
+State rightState = _3x3Search.rightState;
+(int x, int y)[] rightPos = new (int, int)[9];
+for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+        rightPos[rightState[i, j]] = (i, j);
+    }
+}
+_3x3Search t = new([[5, 1, 3], [2, 6, 8], [4, 7, 0]], F);
+Stopwatch sw = Stopwatch.StartNew();
+var path = t.Search();
+sw.Stop();
+Console.WriteLine($"Done! 用时{sw.Elapsed}，以下是还原步骤：");
+foreach (var item in path) {
+    Console.WriteLine(item);
+    Console.WriteLine("---");
+}
+
+
+int F(State state) {
+    // 0--8
+    // 0: 空缺位置
+
+    int dis = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            dis += Math.Abs(i - rightPos[state[i, j]].x) + Math.Abs(j - rightPos[state[i, j]].y);
+        }
+    }
+    return dis;
+}
