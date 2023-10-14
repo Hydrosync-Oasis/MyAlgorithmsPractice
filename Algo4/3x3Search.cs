6eff5134cef21326b5b506588b509c07a9842e1e
HaibaraAi-Sherry <ConanMarisa@qq.com>
2023-10-14 17:59:43 +0800

    nxn华容道

--- Algo4/3x3Search.cs
+++ Algo4/3x3Search.cs
@@ -7,41 +7,52 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Algo4 {
-    internal class _3x3Search {
+    internal class Klotski {
         public delegate int EvaluationFunc(State state);
         EvaluationFunc func;
-        public static State rightState = new((int[][])[[1, 2, 3], [4, 5, 6], [7, 8, 0]]);
+        public State rightState;
+        int N { get => state.N; }
 
-
+        /// <summary>
+        /// 表示当前华容道的棋盘状态
+        /// </summary>
         public class State {
-            private int[] state = new int[9];
+            /// <summary>
+            /// 表明该状态的阶数
+            /// </summary>
+            public int N { get; private set; }
+            public int Step { get; set; }
+
+            private int[] state;
+
             public State(int[][] state) {
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
+                N = state.Length;
+                this.state = new int[N * N];
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
                         this[i, j] = state[i][j];
                     }
                 }
             }
 
             public State(int[] state) {
+                N = (int)Math.Sqrt(state.Length);
                 this.state = state;
             }
 
-            public long GetCode() {
-                long number = 0;
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        number *= 10;
-                        number += this[i, j];
-                    }
+            public int GetCode() {
+                StringBuilder sb = new();
+                for (int i = 0; i < state.Length; i++) {
+                    sb.Append(state[i]);
+                    sb.Append(',');
                 }
-                return number;
+                return sb.ToString().GetHashCode();
             }
 
             public List<State> GenNext() {
                 int x = -1, y = -1;
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
                         if (this[i, j] == 0) {
                             x = i;
                             y = j;
@@ -53,21 +64,28 @@ namespace Algo4 {
                 if (x != 0) {
                     (this[x, y], this[x - 1, y]) = (this[x - 1, y], this[x, y]);
                     ans.Add(this.Copy());
+                    ans[^1].Step++;
                     (this[x, y], this[x - 1, y]) = (this[x - 1, y], this[x, y]);
                 }
-                if (x != 2) {
+                if (x != N - 1) {
                     (this[x, y], this[x + 1, y]) = (this[x + 1, y], this[x, y]);
                     ans.Add(this.Copy());
+                    ans[^1].Step++;
+
                     (this[x, y], this[x + 1, y]) = (this[x + 1, y], this[x, y]);
                 }
                 if (y != 0) {
                     (this[x, y], this[x, y - 1]) = (this[x, y - 1], this[x, y]);
                     ans.Add(this.Copy());
+                    ans[^1].Step++;
+
                     (this[x, y], this[x, y - 1]) = (this[x, y - 1], this[x, y]);
                 }
-                if (y != 2) {
+                if (y != N - 1) {
                     (this[x, y], this[x, y + 1]) = (this[x, y + 1], this[x, y]);
                     ans.Add(this.Copy());
+                    ans[^1].Step++;
+
                     (this[x, y], this[x, y + 1]) = (this[x, y + 1], this[x, y]);
                 }
                 return ans;
@@ -76,16 +94,18 @@ namespace Algo4 {
             public static implicit operator State(int[][] input) => new(input);
 
             public int this[int x, int y] {
-                get => state[3 * x + y];
-                set => state[3 * x + y] = value;
+                get => state[N * x + y];
+                set => state[N * x + y] = value;
             }
 
             public override int GetHashCode() => GetCode().GetHashCode();
 
             public State Copy() {
-                int[] copy = new int[9];
+                int[] copy = new int[N * N];
                 state.CopyTo(copy, 0);
-                return new(copy);
+                State c = new State(copy);
+                c.Step = Step;
+                return c;
             }
 
             public override bool Equals(object? obj) {
@@ -98,10 +118,10 @@ namespace Algo4 {
 
             public override string ToString() {
                 StringBuilder sb = new();
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
                         sb.Append(this[i, j]);
-                        sb.Append(' ');
+                        sb.Append('\t');
                     }
                     sb.AppendLine();
                 }
@@ -111,28 +131,46 @@ namespace Algo4 {
 
         State state;
 
-        public _3x3Search(int[][] state, EvaluationFunc func) {
+        public Klotski(State state, EvaluationFunc func) {
             this.state = state;
             this.func = func;
+            int[] sta = new int[N * N];
+            for (int i = 0; i < N * N - 1; i++) {
+                sta[i] = i + 1;
+            }
+            rightState = new(sta);
         }
 
         public List<State> Search() {
             Dictionary<State, State> map = new();
             PriorityQueue<State, int> pq = new();
-            HashSet<State> visited = new HashSet<State>() { state };
-            pq.Enqueue(state, func(state));
+            HashSet<State> closed = new() { };
+
+            Dictionary<State, int> dist = new() { [state] = func(state) };
+            pq.Enqueue(state, dist[state]);
             while (pq.Count > 0) {
                 var tmp = pq.Dequeue();
-                if (tmp.Equals(rightState)) {
+                if (closed.Contains(tmp)) { // 延迟删除
+                    continue;
+                }
+                if (tmp.Equals(rightState)) { // 达到了终点状态
                     break;
                 }
 
+                closed.Add(tmp);
+
                 List<State> nxt = tmp.GenNext();
                 foreach (var item in nxt) {
-                    if (visited.Add(item)) {
-                        map[item] = tmp;
-                        pq.Enqueue(item, func(item));
+                    if (closed.Contains(item)) {
+                        continue;
                     }
+                    int f = func(item);
+                    if (!dist.TryGetValue(item, out int value) || value > f + tmp.Step + 1) {
+                        map[item] = tmp;
+                        dist[item] = f + tmp.Step + 1;
+                        pq.Enqueue(item, f + tmp.Step + 1);
+                    }
+
                 }
             }
 
@@ -140,10 +178,11 @@ namespace Algo4 {
             State cur = rightState;
             while (true) {
                 result.Add(cur);
-                if (!map.ContainsKey(cur)) {
+                if (!map.TryGetValue(cur, out State s)) {
                     break;
+                } else {
+                    cur = s;
                 }
-                cur = map[cur];
             }
             result.Reverse();
             return result;
--- Algo4/Program.cs
+++ Algo4/Program.cs
@@ -1,16 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Algo4;
 using System.Diagnostics;
-using static Algo4._3x3Search;
+using static Algo4.Klotski;
 
-State rightState = _3x3Search.rightState;
-(int x, int y)[] rightPos = new (int, int)[9];
-for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-        rightPos[rightState[i, j]] = (i, j);
+
+State start = new((int[][])[[6,14,15,13],[4,11,1,5],[10,7,2,9],[12,3,8,0]]);
+
+(int x, int y)[] endPos = new (int, int)[start.N * start.N];// 最终状态的位置
+
+Klotski t = new(start, F);
+State rightState = t.rightState;
+// 生成末状态的位置：
+for (int i = 0; i < start.N; i++) {
+    for (int j = 0; j < start.N; j++) {
+        endPos[rightState[i, j]] = (i, j);
     }
 }
-_3x3Search t = new([[5, 1, 3], [2, 6, 8], [4, 7, 0]], F);
+
+
 Stopwatch sw = Stopwatch.StartNew();
 var path = t.Search();
 sw.Stop();
@@ -19,17 +26,24 @@ foreach (var item in path) {
     Console.WriteLine(item);
     Console.WriteLine("---");
 }
-
+Console.WriteLine($"步骤数：{path.Count}");
 
 int F(State state) {
     // 0--8
     // 0: 空缺位置
 
+    //h(x):
     int dis = 0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            dis += Math.Abs(i - rightPos[state[i, j]].x) + Math.Abs(j - rightPos[state[i, j]].y);
+    for (int i = 0; i < state.N; i++) {
+        for (int j = 0; j < state.N; j++) {
+            if (state[i, j] == 0) {
+                continue;
+            }
+            // g:
+            dis += Math.Abs(i - endPos[state[i, j]].x) + Math.Abs(j - endPos[state[i, j]].y);
+
         }
     }
+
     return dis;
 }
