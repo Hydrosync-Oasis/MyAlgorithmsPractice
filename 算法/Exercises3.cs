72cb599f9f846839840c68c3e5e6964d254534b1
HaibaraAi-Sherry <ConanMarisa@qq.com>
2023-11-06 22:47:51 +0800

    update codes

--- 算法/Exercises3.cs
+++ 算法/Exercises3.cs
@@ -409,5 +409,47 @@ namespace Algorithm {
             return res;
         }
 
+        public static int MaxDotProduct(int[] nums1, int[] nums2) {
+            int n = nums1.Length;
+            int m = nums2.Length;
+            int[,] dp = new int[n, m]; // 包含i和j的乘积的最大点积
+            int[,] dp2 = new int[n, m];// 记录max
+            dp[0, 0] = dp2[0, 0] = nums1[0] * nums2[0];
+            for (int i = 1; i < n; i++) {
+                dp[i, 0] = nums1[i] * nums2[0];
+                dp2[i, 0] = Math.Max(dp2[i - 1, 0], dp[i, 0]);
+            }
+            for (int i = 1; i < m; i++) {
+                dp[0, i] = nums1[0] * nums2[i];
+                dp2[0, i] = Math.Max(dp2[0, i - 1], dp[0, i]);
+            }
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j < m; j++) {
+                    dp[i, j] = nums1[i] * nums2[j] + Math.Max(0, dp2[i - 1, j - 1]);
+                    dp2[i, j] = Math.Max(dp2[i - 1, j], dp2[i, j - 1]);
+                    dp2[i, j] = Math.Max(dp2[i, j], dp[i, j]);
+                }
+            }
+
+            Print2DArray(dp);
+
+            Print2DArray(dp2);
+            return dp2[n - 1, m - 1];
+        }
+
+        static void Print2DArray(int[,] array) {
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---");
+        }
     }
+
+
 }
